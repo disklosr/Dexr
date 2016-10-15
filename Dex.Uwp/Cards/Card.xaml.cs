3a938d22b71252efc9249bd6106f3745318815ea
Anas Mazioudi <disklosr@outlook.com>
2016-10-15 16:06:05 +0200

    Add shadow to list items + new color palette

--- Dex.Uwp/Cards/Card.xaml.cs
+++ Dex.Uwp/Cards/Card.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Dex.Uwp.Theme;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
 
 namespace Dex.Uwp.Cards
 {
@@ -7,6 +11,43 @@ namespace Dex.Uwp.Cards
         public Card()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            var source = (Grid)GetTemplateChild("CardRoot");
+            var target = (Canvas)GetTemplateChild("ShadowHost");
+
+            InitializeDropShadow(source, target);
+
+            base.OnApplyTemplate();
+        }
+
+        private void InitializeDropShadow(UIElement shadowShape, UIElement shadowHost)
+        {
+            Visual hostVisual = ElementCompositionPreview.GetElementVisual(shadowShape);
+            Compositor compositor = hostVisual.Compositor;
+
+            // Create a drop shadow
+            var dropShadow = compositor.CreateDropShadow();
+
+            var shadowStyle = (DropShadowStyle)Application.Current.Resources["DropShadowStyle"];
+            dropShadow.Color = shadowStyle.Color;
+            dropShadow.BlurRadius = shadowStyle.BlurRadius;
+            //dropShadow.Offset = shadowStyle.Offset;
+
+            // Create a Visual to hold the shadow
+            var shadowVisual = compositor.CreateSpriteVisual();
+            shadowVisual.Shadow = dropShadow;
+
+            // Add the shadow as a child of the host in the visual tree
+            ElementCompositionPreview.SetElementChildVisual(shadowHost, shadowVisual);
+
+            // Make sure size of shadow host and shadow visual always stay in sync
+            var bindSizeAnimation = compositor.CreateExpressionAnimation("hostVisual.Size");
+            bindSizeAnimation.SetReferenceParameter("hostVisual", hostVisual);
+
+            shadowVisual.StartAnimation("Size", bindSizeAnimation);
         }
     }
 }
---
+++ Dex.Uwp/Theme/DropShadowStyle.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+using Windows.UI;
+
+namespace Dex.Uwp.Theme
+{
+    public class DropShadowStyle
+    {
+        public readonly Color Color = Color.FromArgb(100, 0, 0, 0);
+
+        public readonly Vector3 Offset = new Vector3(0f, 2f, 2f);
+        public float BlurRadius = 4.0f;
+    }
+}
