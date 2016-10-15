ac6ff10d77f0c63aa25bea079b9f572b6f8073ac
Anas Mazioudi <disklosr@outlook.com>
2016-10-15 20:53:35 +0200

    First version of pokemon detail card

--- Dex.Uwp/Cards/Card.xaml.cs
+++ Dex.Uwp/Cards/Card.xaml.cs
@@ -10,7 +10,7 @@ namespace Dex.Uwp.Cards
     {
         public Card()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         protected override void OnApplyTemplate()
--- Dex.Uwp/Theme/DropShadowStyle.cs
+++ Dex.Uwp/Theme/DropShadowStyle.cs
@@ -6,7 +6,6 @@ namespace Dex.Uwp.Theme
     public class DropShadowStyle
     {
         public readonly Color Color = Color.FromArgb(120, 0, 0, 0);
-
         public readonly Vector3 Offset = new Vector3(0f, 2f, 2f);
         public float BlurRadius = 4.0f;
     }
