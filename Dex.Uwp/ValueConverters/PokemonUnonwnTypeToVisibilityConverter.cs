bdbdd5a7cd076364823bf85e3665f50c20a8702f
Anas Mazioudi <disklosr@outlook.com>
2016-10-20 22:43:59 +0200

    Add display of pokemon type

---
+++ Dex.Uwp/ValueConverters/PokemonUnonwnTypeToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Dex.Uwp.ValueConverters
+{
+    public class PokemonUnonwnTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((Core.Entities.Type)value) == Core.Entities.Type.Unknown ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
