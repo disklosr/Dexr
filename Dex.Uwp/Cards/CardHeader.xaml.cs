7b8ce161685ee5d02f88c126fdfe553716bbe422
Anas Mazioudi <disklosr@outlook.com>
2016-10-22 23:15:48 +0200

    Create & use card header control

---
+++ Dex.Uwp/Cards/CardHeader.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Dex.Uwp.Cards
+{
+    public sealed partial class CardHeader : ContentControl
+    {
+        private static DependencyProperty TitleProperty
+       = DependencyProperty.Register("Title", typeof(string), typeof(CardHeader), new PropertyMetadata(string.Empty));
+
+        public CardHeader()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
