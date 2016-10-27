1a6cafe903b012dbbc918f39443a522b878a5e0d
Anas Mazioudi <disklosr@outlook.com>
2016-10-27 18:48:52 +0200

    Add nice navigation animations

--- Dex.Uwp/Theme/DropShadowStyle.cs
+++ Dex.Uwp/Theme/DropShadowStyle.cs
@@ -5,7 +5,7 @@ namespace Dex.Uwp.Theme
 {
     public class DropShadowStyle
     {
-        public readonly Color Color = Color.FromArgb(120, 0, 0, 0);
+        public readonly Color Color = Color.FromArgb(96, 0, 0, 0);
         public readonly Vector3 Offset = new Vector3(0f, 2f, 2f);
         public float BlurRadius = 4.0f;
     }
