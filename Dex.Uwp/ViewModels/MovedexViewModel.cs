9eeffdcf5db6b60464e6a1cd830308aad0f36e72
disklosr <disklosr@outlook.com>
2016-11-09 22:53:05 +0100

    Wire list selection to view model in movedex page

--- Dex.Uwp/ViewModels/MovedexViewModel.cs
+++ Dex.Uwp/ViewModels/MovedexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Dex.Core.Entities;
 using Dex.Core.Repositories;
 using Dex.Uwp.Infrastructure;
+using Dex.Uwp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,13 +15,15 @@ namespace Dex.Uwp.ViewModels
     public class MovedexViewModel : ViewModelBase
     {
         private readonly IMoveRepository moveRepository;
+        private readonly INavigationService navigationService;
         private IEnumerable<ChargeMove> allChargeMovesById;
         private PokemonMoves allMoves;
 
         private IEnumerable<QuickMove> allQuickMovesById;
 
-        public MovedexViewModel(IMoveRepository moveRepository)
+        public MovedexViewModel(IMoveRepository moveRepository, INavigationService navigationService)
         {
+            this.navigationService = navigationService;
             this.moveRepository = moveRepository;
             ReverseOrderCommand = new RelayCommand(() => OnReverseOrder());
         }
@@ -38,6 +41,11 @@ namespace Dex.Uwp.ViewModels
         }
 
         public ICommand ReverseOrderCommand { get; }
+
+        public void OnMoveSelected(Move selectedMove)
+        {
+            navigationService.NavigateToMoveDetailsPage(selectedMove.MoveId);
+        }
 
         public async override Task OnNavigatedTo(NavigationEventArgs e)
         {
