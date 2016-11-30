8cf8791d2962be59c6150d8186c73d261091a70b
disklosr <disklosr@outlook.com>
2016-11-30 23:24:07 +0100

    Miscellaneous improvements

--- Dex.Uwp/ViewModels/AboutViewModel.cs
+++ Dex.Uwp/ViewModels/AboutViewModel.cs
@@ -1,4 +1,8 @@
+using System;
 using Dex.Uwp.Infrastructure;
+using Microsoft.Services.Store.Engagement;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Windows.ApplicationModel;
 
 namespace Dex.Uwp.ViewModels
@@ -10,7 +14,18 @@ namespace Dex.Uwp.ViewModels
         public AboutViewModel()
         {
             _appVersion = Package.Current.Id.Version;
+            SendFeedbackCommand = new RelayCommand(async () => await OnSendFeedback());
         }
         public string AppVersion => string.Format("{0}.{1}.{2}.{3}", _appVersion.Major, _appVersion.Minor, _appVersion.Build, _appVersion.Revision);
+
+        public ICommand SendFeedbackCommand { get; }
+
+        private async Task OnSendFeedback()
+        {
+            var launcher = StoreServicesFeedbackLauncher.GetDefault();
+            await launcher.LaunchAsync();
+        }
+
+        public bool CanSendFeedback => StoreServicesFeedbackLauncher.IsSupported();
     }
 }
