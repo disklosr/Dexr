8ea76e083423de63e462958401167a2e02019c45
disklosr <disklosr@outlook.com>
2017-01-07 18:39:55 +0100

    v 1.2.0.0

--- Dex.Uwp/Properties/AssemblyInfo.cs
+++ Dex.Uwp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dex.Uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright @disklosr 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
