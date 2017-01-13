278f866ec6d09188b541bccbe88035e133061ef5
disklosr <disklosr@outlook.com>
2017-01-13 13:29:41 +0100

    Add rate this app entry to side pane menu

--- Dex.Uwp/Pages/SidePane.xaml.cs
+++ Dex.Uwp/Pages/SidePane.xaml.cs
@@ -2,22 +2,22 @@
 using Microsoft.Practices.ServiceLocation;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Windows.UI.Xaml.Controls;
 
 namespace Dex.Uwp.Pages
 {
     public sealed partial class SidePane : UserControl
     {
-        private Dictionary<string, Action> MenuItemToPage;
-        private INavigationService navigationService;
+        private Dictionary<string, Action> _menuItemToPage;
+        private INavigationService _navigationService;
+        private IStoreService _storeService;
 
         public SidePane()
         {
             InitializeComponent();
             InitMenuItemToPage();
 
-            MyList.ItemsSource = MenuItemToPage.Keys;
+            MyList.ItemsSource = _menuItemToPage.Keys;
             MyList.ItemClick += MyList_ItemClick;
             Loaded += SidePane_Loaded;
         }
@@ -25,26 +25,28 @@ namespace Dex.Uwp.Pages
         private void InitMenuItemToPage()
         {
             //TODO: Make these automaticly resolved to avoid adding new entries each time
-            MenuItemToPage = new Dictionary<string, Action>()
+            _menuItemToPage = new Dictionary<string, Action>()
             {
-                ["Pokedex"] = () => navigationService.NavigateToPokedexPage(),
-                ["Movedex"] = () => navigationService.NavigateToMovesPage(),
-                ["Types"] = () => navigationService.NavigateToTypesPage(),
-                ["Settings"] = () => navigationService.NavigateToSettingsPage(),
-                ["About"] = () => navigationService.NavigateToMoveAboutPage()
+                ["Pokedex"] = () => _navigationService.NavigateToPokedexPage(),
+                ["Movedex"] = () => _navigationService.NavigateToMovesPage(),
+                ["Types"] = () => _navigationService.NavigateToTypesPage(),
+                ["Settings"] = () => _navigationService.NavigateToSettingsPage(),
+                ["About"] = () => _navigationService.NavigateToMoveAboutPage(),
+                ["Rate This App"] = () => _storeService.RateThisApp()
             };
         }
 
         private void MyList_ItemClick(object sender, ItemClickEventArgs e)
         {
-            var currentPage = navigationService.CurrentPage.Name.Replace("Page", string.Empty);
+            var currentPage = _navigationService.CurrentPage.Name.Replace("Page", string.Empty);
             if ((string)e.ClickedItem != currentPage)
-                MenuItemToPage[(string)e.ClickedItem].Invoke();
+                _menuItemToPage[(string)e.ClickedItem].Invoke();
         }
 
         private void SidePane_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            navigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+            _navigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+            _storeService = ServiceLocator.Current.GetInstance<IStoreService>();
         }
     }
 }
